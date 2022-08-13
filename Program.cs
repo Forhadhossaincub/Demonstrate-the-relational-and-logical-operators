c8c48834de3578cee315ae78063e52cfc7dfb173
FORHAD HOSSAIN <forhadhossaincub@gmail.com>
2022-08-13 23:39:38 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demonstrate_the_relational_and_logical_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+
+            int i, j;
+            bool b1, b2;
+
+            i = 5;
+            j = 6;
+
+            if (i < j) Console.WriteLine("i < j");
+            if (i <= j) Console.WriteLine("i <= j");
+            if (i != j) Console.WriteLine("i != j");
+            if (i == j) Console.WriteLine("this won't execute");
+            if (i >= j) Console.WriteLine("this won't execute");
+            if (i > j) Console.WriteLine("this won't execute");
+
+            b1 = true;
+            b2 = false;
+            if (b1 & b2) Console.WriteLine("this won't execute");
+            if (!(b1 & b2)) Console.WriteLine("!(b1 & b2) is true");
+            if (b1 | b2) Console.WriteLine("b1 | b2 is true");
+            if (b1 ^ b2) Console.WriteLine("b1 ^ b2 is true");
+
+            Console.ReadKey();
+
+        }
+    }
+}
